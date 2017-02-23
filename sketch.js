69085ebef90752ccecf79dfdfc6d46d950809560
ericjduran <ace_e303@e303-s11.lab.lagcc>
2017-02-23 11:37:12 -0500

    project_ce

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  // creating canvas and the background
+  createCanvas(1280, 1080);
+  background(51);
+}
+
+function draw() {
+ //color white for circle so it maintains its color
+ fill(255, 255, 255);
+ ellipse(150, 200, 250, 250);
+ 
+ //Pyramid size, position, Color of pyramid
+ fill(240, 230, 140);
+ triangle(130, 1075, 658, 620, 1080, 1075);
+ //line in pyramid
+ line(780, 780, 780, 1060)
+}
+//Eric drove Chris typed
